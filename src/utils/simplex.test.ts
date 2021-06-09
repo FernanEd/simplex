bfcc687d8c997bc6bc394badabe381a858c3695b
fernaned <fernandoed.bm@gmail.com>
2021-06-09 15:28:10 -0500

    simplex working now, data-refactoring from user input is the problem now

--- src/utils/simplex.test.ts
+++ src/utils/simplex.test.ts
@@ -1,10 +1,9 @@
 import simplex, {
-	faseUno,
+	// faseUno,
 	fixNumbers,
 	getPivots,
-	iteracion,
-	iteration,
-	sacarPivotes,
+	// iteration,
+	// phaseTwo,
 } from './simplex';
 
 const smallMatrix = [
@@ -52,59 +51,108 @@ const largeMatrixBasica = [
 ];
 
 test('simplex in the house', () => {
-	expect(simplex(smallMatrix, [6], [0, 1], [-800, -600, 0, 0, 0, 0, 0])).toBe(
-		3
-	);
+	expect(
+		simplex(
+			smallMatrix,
+			[6],
+			[0, 1],
+			[-800, -600, 0, 0, 0, 0, 0],
+			['x1', 'x2', 's1', 's2', 's3', 'e1', 'r1'],
+			['s1', 's2', 's3', 'r1', 'z']
+		)
+	).toEqual({
+		x2: 5,
+		s2: 6,
+		s3: 3,
+		x1: 4,
+		z: 6200,
+	});
 });
 
 test('simplex in the house', () => {
-	expect(simplex(largeMatrix, [5, 6], [0, 1, 2], [-2, -3, -1, 0, 0, 0])).toBe(
-		3
-	);
-});
-
-test('fase uno funciona', () => {
-	expect(faseUno(largeMatrix, [5, 6])).toEqual([
-		[0, 1, 0],
-		[1, 0, 0],
-		[0.6, -0.4, 0],
-		[-0.3, 0.2, 0],
-		[0.1, -0.4, 0],
-		[0.3, -0.2, -1],
-		[-0.1, 0.4, -1],
-		[1.8, 0.8, 0],
-	]);
-});
-
-test('iteracion funciona', () => {
 	expect(
-		iteration(smallMatrixBasic).map((col) =>
-			col.map((value) => Number(value.toPrecision(4)))
+		simplex(
+			largeMatrix,
+			[5, 6],
+			[0, 1, 2],
+			[-2, -3, -1, 0, 0, 0],
+			['x1', 'x2', 'x3', 'e1', 'e2', 'r1', 'r2'],
+			['r1', 'r2', 'z']
 		)
-	).toEqual([
-		[0, 0, 0, 1, 0],
-		[0.2, -0.8, 1, 0.8, 0],
-		[1, 0, 0, 0, 0],
-		[0, 1, 0, 0, 0],
-		[0, 0, 1, 0, 0],
-		[0.02, 0.02, 0, -0.02, 0],
-		[-0.02, -0.02, 0, 0.02, -1],
-		[1, 2, 8, 8, 0],
-	]);
+	).toEqual({
+		x2: 1.8,
+		x1: 0.8,
+		z: 7,
+	});
 });
 
-test('iteracion funciona', () => {
-	expect(iteration(largeMatrixBasica)).toEqual([
-		[0.25, 2.5, 2.5],
-		[1, 0, 0],
-		[0.5, -1, -1],
-		[-0.25, 0.5, 0.5],
-		[0, -1, -1],
-		[0.25, -0.5, -1.5],
-		[0, 1, 0],
-		[2, 2, 2],
-	]);
-});
+// test('fase dos funciona', () => {
+// 	expect(
+// 		phaseTwo(
+// 			[
+// 				[0, 0, 0, 1, -800],
+// 				[0.2, -0.8, 1, 0.8, -600],
+// 				[1, 0, 0, 0, 0],
+// 				[0, 1, 0, 0, 0],
+// 				[0, 0, 1, 0, 0],
+// 				[0.02, 0.02, 0, -0.02, 0],
+// 				[1, 2, 8, 8, 0],
+// 			],
+// 			[0, 1, 2]
+// 		).map((columna) => columna.map((valor) => fixNumbers(valor)))
+// 	).toEqual([
+// 		[0, 0, 0, 1, 0],
+// 		[1, 0, 0, 0, 0],
+// 		[5, 4, -5, -4, -200],
+// 		[0, 1, 0, 0, 0],
+// 		[0, 0, 1, 0, 0],
+// 		[0.1, 0.1, -0.1, -0.1, -20],
+// 		[5, 6, 3, 4, 6200],
+// 	]);
+// });
+
+// test('fase uno funciona', () => {
+// 	expect(faseUno(largeMatrix, [5, 6])).toEqual([
+// 		[0, 1, 0],
+// 		[1, 0, 0],
+// 		[0.6, -0.4, 0],
+// 		[-0.3, 0.2, 0],
+// 		[0.1, -0.4, 0],
+// 		[0.3, -0.2, -1],
+// 		[-0.1, 0.4, -1],
+// 		[1.8, 0.8, 0],
+// 	]);
+// });
+
+// test('iteracion funciona', () => {
+// 	expect(
+// 		iteration(smallMatrixBasic).map((col) =>
+// 			col.map((value) => Number(value.toPrecision(4)))
+// 		)
+// 	).toEqual([
+// 		[0, 0, 0, 1, 0],
+// 		[0.2, -0.8, 1, 0.8, 0],
+// 		[1, 0, 0, 0, 0],
+// 		[0, 1, 0, 0, 0],
+// 		[0, 0, 1, 0, 0],
+// 		[0.02, 0.02, 0, -0.02, 0],
+// 		[-0.02, -0.02, 0, 0.02, -1],
+// 		[1, 2, 8, 8, 0],
+// 	]);
+// });
+
+// test('iteracion funciona', () => {
+// 	expect(iteration(largeMatrixBasica)).toEqual([
+// 		[0.25, 2.5, 2.5],
+// 		[1, 0, 0],
+// 		[0.5, -1, -1],
+// 		[-0.25, 0.5, 0.5],
+// 		[0, -1, -1],
+// 		[0.25, -0.5, -1.5],
+// 		[0, 1, 0],
+// 		[2, 2, 2],
+// 	]);
+// });
 
 test('sacar pivotes funciona', () => {
 	expect(getPivots(smallMatrixBasic)).toEqual({
--- src/utils/simplex.ts
+++ src/utils/simplex.ts
@@ -45,152 +45,172 @@ export const getPivots = (matriz: simplexMatriz) => {
 	return { pivotColumnIndx, pivotRowIndx };
 };
 
-export const iteration = (matrix: simplexMatriz) => {
-	let m = [...matrix];
-	let { pivotColumnIndx, pivotRowIndx } = getPivots(m);
-	let intersection = m[pivotColumnIndx][pivotRowIndx];
-
-	let pivotRow = m.map((columna) =>
-		safeDivision(columna[pivotRowIndx], intersection)
-	);
-
-	let pivotColumn = m[pivotColumnIndx];
-
-	m = m.map((column, i) =>
-		column.map((value, j) => {
-			if (j === pivotRowIndx) {
-				return pivotRow[i];
-			} else {
-				return value - pivotColumn[j] * pivotRow[i];
-			}
-		})
-	);
-
-	// console.log(indiceRenglonPivote);
-	// console.log(renglonPivote);
-	// console.log(indiceColumnaPivote);
-	// console.log(columnaPivote);
-	// console.log(m);
-
-	return m;
-};
-
-export const faseUno = (
-	matriz: simplexMatriz,
-	indicesVariablesR: simplexVariableR
-) => {
-	let m = [...matriz];
-
-	const columnasVariablesR = m.filter((_, i) => indicesVariablesR.includes(i));
-
-	[...columnasVariablesR].flat();
-
-	const indicesColumnasSumar = columnasVariablesR
-		.map((columna) => columna.indexOf(1))
-		.flat();
-
-	m = m.map((columna) =>
-		columna.map((val, i) => {
-			if (i === columna.length - 1) {
-				return (
-					val +
-					indicesColumnasSumar
-						.map((indice) => columna[indice])
-						.reduce((a, b) => a + b, 0)
-				);
-			} else {
-				return val;
-			}
-		})
-	);
-
-	let m2 = m;
-	let isRPositive;
-	let iterations = 0;
-
-	do {
-		m2 = iteration(m2);
-		const lastRow = getLastRow(m2).slice(0, -1);
-		isRPositive = lastRow.some((value) => value > 0);
-		iterations++;
-	} while (isRPositive && iterations < 50);
-
-	if (iterations >= 50) {
-		throw Error(
-			`Demasiadas iteraciones, hay un problema con la matriz: ${matriz}`
-		);
-	}
-
-	return m2;
-};
-
 const simplex = (
 	matriz: simplexMatriz,
 	indicesVariablesR: simplexVariableR,
 	indicesVariables: simplexVariable,
-	renglonZ: simplexRenglon
+	renglonZ: simplexRenglon,
+	columnHeaders: string[],
+	rowHeaders: string[]
 ) => {
-	const m1 = faseUno(matriz, indicesVariablesR).filter(
-		(_, i) => !indicesVariablesR.includes(i)
-	);
+	const iteration = (matrix: simplexMatriz) => {
+		let m = [...matrix];
+		let { pivotColumnIndx, pivotRowIndx } = getPivots(m);
+		let intersection = m[pivotColumnIndx][pivotRowIndx];
 
-	let m = m1.map((columna, i) =>
-		columna.map((value, j) => (j === columna.length - 1 ? renglonZ[i] : value))
-	);
+		let pivotRow = m.map((columna) =>
+			safeDivision(columna[pivotRowIndx], intersection)
+		);
 
-	for (let indiceVar of indicesVariables) {
-		let columna = m[indiceVar];
-		let indiceRenglonPseudoPivote = columna.indexOf(1);
-		let interseccion = columna[columna.length - 1];
+		let pivotColumn = m[pivotColumnIndx];
 
-		//Multiplicar toda la tabla
-		m = m.map((columna, i) =>
-			columna.map((valor, j) => {
-				if (j === columna.length - 1) {
-					return fixNumbers(
-						valor - interseccion * columna[indiceRenglonPseudoPivote]
-					);
+		m = m.map((column, i) =>
+			column.map((value, j) => {
+				if (j === pivotRowIndx) {
+					return pivotRow[i];
 				} else {
-					return valor;
+					return value - pivotColumn[j] * pivotRow[i];
 				}
 			})
 		);
 
-		let variablesX = m
-			.map((columna) => columna[columna.length - 1])
-			.filter((_, i) => indicesVariables.includes(i));
+		rowHeaders[pivotRowIndx] = columnHeaders[pivotColumnIndx];
 
-		let negativosEnVariables = variablesX.some((value) => value < 0);
+		// console.log(indiceRenglonPivote);
+		// console.log(renglonPivote);
+		// console.log(indiceColumnaPivote);
+		// console.log(columnaPivote);
+		// console.log(m);
 
-		// console.log(variablesX, negativosEnVariables);
+		return m;
+	};
 
-		if (!negativosEnVariables) {
-			break;
+	const faseUno = (
+		matriz: simplexMatriz,
+		indicesVariablesR: simplexVariableR
+	) => {
+		let m = [...matriz];
+
+		const columnasVariablesR = m.filter((_, i) =>
+			indicesVariablesR.includes(i)
+		);
+
+		const indicesColumnasSumar = columnasVariablesR
+			.map((columna) => columna.indexOf(1))
+			.flat();
+
+		m = m.map((columna) =>
+			columna.map((val, i) => {
+				if (i === columna.length - 1) {
+					return (
+						val +
+						indicesColumnasSumar
+							.map((indice) => columna[indice])
+							.reduce((a, b) => a + b, 0)
+					);
+				} else {
+					return val;
+				}
+			})
+		);
+
+		let m2 = m;
+		let isRPositive;
+		let iterations = 0;
+
+		do {
+			m2 = iteration(m2);
+			const lastRow = getLastRow(m2).slice(0, -1);
+			isRPositive = lastRow.some((value) => value > 0);
+			iterations++;
+		} while (isRPositive && iterations < 50);
+
+		if (iterations >= 50) {
+			throw Error(
+				`Demasiadas iteraciones, hay un problema con la matriz: ${matriz}`
+			);
 		}
 
-		// console.log(indiceVar);
-		// console.log(interseccion);
-		// console.log(columna);
-		// console.log(indiceRenglonPseudoPivote);
-		// console.log(m);
-	}
+		return m2;
+	};
 
-	// FORMA BASICA FASE 2
+	const phaseTwo = (
+		matriz: simplexMatriz,
+		indicesVariables: simplexVariable
+	) => {
+		let m = [...matriz];
+		for (let indiceVar of indicesVariables) {
+			let columna = m[indiceVar];
+			let indiceRenglonPseudoPivote = columna.indexOf(1);
+			let interseccion = columna[columna.length - 1];
 
-	m;
-	let iteraciones = 0;
-	let isZPositive;
+			//Multiplicar toda la tabla
+			m = m.map((columna, i) =>
+				columna.map((valor, j) => {
+					if (j === columna.length - 1) {
+						return fixNumbers(
+							valor - interseccion * columna[indiceRenglonPseudoPivote]
+						);
+					} else {
+						return valor;
+					}
+				})
+			);
 
-	do {
-		m = iteration(m);
-		const lastRow = getLastRow(m).slice(0, -1);
-		isZPositive = lastRow.some((value) => value > 0);
-		iteraciones++;
-	} while (isZPositive && iteraciones < 50);
+			let variablesX = m
+				.map((columna) => columna[columna.length - 1])
+				.filter((_, i) => indicesVariables.includes(i));
 
-	console.log(m);
-	// console.log(variablesX);
-	// console.log(negativosEnVariables);
+			let negativosEnVariables = variablesX.some((value) => value < 0);
+
+			if (!negativosEnVariables) {
+				break;
+			}
+		}
+
+		let m2 = m;
+		let iterations = 0;
+		let isZPositive;
+
+		do {
+			m2 = iteration(m2);
+			const lastRow = getLastRow(m2).slice(0, -1);
+			isZPositive = lastRow.some((value) => value > 0);
+			iterations++;
+		} while (isZPositive && iterations < 50);
+
+		if (iterations >= 50) {
+			throw Error(
+				`Demasiadas iteraciones, hay un problema con la matriz: ${matriz}`
+			);
+		}
+
+		return m2;
+	};
+
+	//Get phase two basic table (minus R vars)
+	const m1 = faseUno(matriz, indicesVariablesR).filter(
+		(_, i) => !indicesVariablesR.includes(i)
+	);
+
+	//Replace r row with Z row
+	let m = m1.map((columna, i) =>
+		columna.map((value, j) => (j === columna.length - 1 ? renglonZ[i] : value))
+	);
+
+	let m2 = phaseTwo(m, indicesVariables);
+	let results = m2[m2.length - 1];
+
+	// console.log(results);
+	// console.log(rowHeaders);
+
+	let solution: { [x: string]: number } = {};
+
+	rowHeaders.forEach((header, i) => {
+		solution[header] = fixNumbers(results[i]);
+	});
+
+	return solution;
 };
 
 export default simplex;
