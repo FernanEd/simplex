036ffb361abd102e81812d1bac5680acd95e6afb
fernaned <fernandoed.bm@gmail.com>
2021-06-16 20:22:03 -0500

    added iteration function and fixed getPivots to cut out result column

--- src/utils/simplexMethod.test.ts
+++ src/utils/simplexMethod.test.ts
@@ -1,4 +1,8 @@
-import simplexMethod from './simplexMethod';
+import simplexMethod, {
+	getColumn,
+	getPivots,
+	iteration,
+} from './simplexMethod';
 
 const m1 = {
 	matrix: [
@@ -44,6 +48,36 @@ const m4 = {
 	rowHeaders: ['s1', 's2', 's3', 'r1', 'z'],
 };
 
+const m3b = {
+	matrix: [
+		[1, 4, 2, -1, 0, 1, 0, 8],
+		[3, 2, 0, 0, -1, 0, 1, 6],
+		[4, 6, 2, -1, -1, 0],
+	],
+	columnHeaders: ['x1', 'x2', 'x3', 'e1', 'e2', 'r1', 'r2', 'res'],
+	rowHeaders: ['r1', 'r2', 'z'],
+};
+
+const m4b = {
+	matrix: [
+		[1, 1, 1, 0, 0, 0, 0, 9],
+		[1, 0, 0, 1, 0, 0, 0, 10],
+		[0, 1, 0, 0, 1, 0, 0, 8],
+		[50, 40, 0, 0, 0, -1, 1, 400],
+		[50, 40, 0, 0, -1, 0, 0],
+	],
+	columnHeaders: ['x1', 'x2', 's1', 's2', 's3', 'e1', 'r1', 'res'],
+	rowHeaders: ['s1', 's2', 's3', 'r1', 'z'],
+};
+
+const matrixmin2 = [
+	[1, 1, 1, 0, 0, 0, 0, 210000],
+	[1, 0, 0, 1, 0, 0, 0, 130000],
+	[1, -2, 0, 0, 1, 0, 0, 0],
+	[0, 1, 0, 0, 0, -1, 1, 60000],
+	[0, 1, 0, 0, 0, -1, 0, 60000],
+];
+
 test('', () => {
 	expect(simplexMethod(m1)).toBe(2);
 });
@@ -58,4 +92,62 @@ test('', () => {
 
 test('', () => {
 	expect(simplexMethod(m4)).toBe(1);
+});
+
+test('iteration works', () => {
+	expect(iteration(matrixmin2, 'min')).toEqual([
+		[1, 0, 1, 0, 0, 1, -1, 150000],
+		[1, 0, 0, 1, 0, 0, 0, 130000],
+		[1, 0, 0, 0, 1, -2, 2, 120000],
+		[0, 1, 0, 0, 0, -1, 1, 60000],
+		[0, 0, 0, 0, 0, 0, -1, 0],
+	]);
+});
+
+test('getPivots works', () => {
+	expect(getPivots(matrixmin2, 'min')).toEqual({
+		columnPivotIndex: 1,
+		rowPivotIndex: 3,
+	});
+});
+
+test('iteration works', () => {
+	expect(iteration(m2.matrix, 'max')).toEqual([
+		[0.666667, 1, 0.333333, 0, 0, 200],
+		[0.333333, 0, -0.333333, 1, 0, 300],
+		[1.33333, 0, -0.333333, 0, 1, 200],
+		[-1.83333, 0, 2.33333, 0, 0, 1400],
+	]);
+});
+
+test('getPivots works', () => {
+	expect(getPivots(m1.matrix, 'max')).toEqual({
+		columnPivotIndex: 1,
+		rowPivotIndex: 0,
+	});
+});
+
+test('getPivots works', () => {
+	expect(getPivots(m2.matrix, 'max')).toEqual({
+		columnPivotIndex: 1,
+		rowPivotIndex: 0,
+	});
+});
+
+test('getPivots works', () => {
+	expect(getPivots(m3b.matrix, 'min')).toEqual({
+		columnPivotIndex: 1,
+		rowPivotIndex: 0,
+	});
+});
+
+test('getPivots works', () => {
+	expect(getPivots(m4b.matrix, 'min')).toEqual({
+		columnPivotIndex: 0,
+		rowPivotIndex: 3,
+	});
+});
+
+test('getColumn works', () => {
+	expect(getColumn(m1.matrix, 5)).toEqual([5000, 4500, 0, 0]);
 });
--- src/utils/simplexMethod.ts
+++ src/utils/simplexMethod.ts
@@ -1,26 +1,101 @@
 type matrix = number[][];
 type header = string[];
+type fnZ = 'min' | 'max';
 
 interface simplexParams {
 	matrix: matrix;
 	columnHeaders: header;
 	rowHeaders: header;
+	fnZ: fnZ;
 }
 
-const fixMatriz = (matrix: matrix) =>
+export const fixMatrix = (matrix: matrix) =>
 	matrix.map((column) =>
 		column.map((val) => Number(Number(val.toFixed(6)).toPrecision(6)))
 	);
 
-const getPivots = (matrix: matrix, search: 'min' | 'max') => {};
+export const getColumn = (matriz: matrix, columnIndex: number) => {
+	return matriz.map((row) => row[columnIndex]);
+};
+
+export const safeDivision = (coeficient: number, divisor: number) => {
+	if (divisor === 0) return 0;
+	else {
+		return coeficient / divisor;
+	}
+};
+
+export const copyArray = (matrix: matrix) => {
+	let m = [];
+	for (let i = 0; i < matrix.length; i++) {
+		let row = [];
+		for (let j = 0; j < matrix[0].length; j++) {
+			row.push(matrix[i][j]);
+		}
+		m.push(row);
+	}
+	return m;
+};
+
+export const getPivots = (matrix: matrix, fnZ: fnZ) => {
+	//Search in the last row, depending of fnZ the lowest negative or the higher positive
+	let m = copyArray(matrix);
+	//Get last row but cut out the result column
+	let lastRow = m[m.length - 1].slice(0, -1);
+	let columnPivotIndex = lastRow.indexOf(
+		fnZ == 'min'
+			? Math.max(...lastRow.filter((val) => val > 0))
+			: Math.min(...lastRow.filter((val) => val < 0))
+	);
+
+	//Then divide result column by pivot column and get the row index of the lower positive
+	let resultColumn = getColumn(m, m[0].length - 1);
+	let pivotColumn = getColumn(m, columnPivotIndex);
+	let auxColumn = resultColumn.map((result, i) =>
+		safeDivision(result, pivotColumn[i])
+	);
+
+	let rowPivotIndex = auxColumn.indexOf(
+		Math.min(...auxColumn.filter((val) => val > 0))
+	);
+
+	return {
+		columnPivotIndex,
+		rowPivotIndex,
+	};
+};
+
+export const iteration = (matrix: matrix, fnZ: fnZ) => {
+	let m = copyArray(matrix);
+	let { columnPivotIndex, rowPivotIndex } = getPivots(m, fnZ);
+	let intersectionCoeficient = m[rowPivotIndex][columnPivotIndex];
+
+	let pivotRow = m[rowPivotIndex].map((val) =>
+		safeDivision(val, intersectionCoeficient)
+	);
+
+	// Make a new matrix, when the row is the pivot row, substitute with pivotRow
+	// When not, get the value minus the pivot coeficient multiplied by the respective pivot row of the current column
+	for (let row = 0; row < m.length; row++) {
+		for (let col = 0; col < m[0].length; col++) {
+			m[row][col] =
+				row == rowPivotIndex
+					? pivotRow[col]
+					: matrix[row][col] - matrix[row][columnPivotIndex] * pivotRow[col];
+		}
+	}
+
+	return fixMatrix(m);
+};
 
 const simplexMethod = ({
 	matrix,
 	columnHeaders,
 	rowHeaders,
+	fnZ,
 }: simplexParams) => {
 	const twoPhases = columnHeaders.includes('r1');
-	let m = [...matrix];
+	let m = copyArray(matrix);
 
 	if (twoPhases) {
 		return 1;
@@ -33,7 +108,7 @@ const simplexMethod = ({
 
     First phase: 
     Save the z row
-    Override it with a new row where everything is 0 
+    Override it with a new r row where everything is 0 
     except the r columns which are -1
     Solve for a basic solution: find the columns where r is -1.
     Find the rows where the column is 1
@@ -42,19 +117,22 @@ const simplexMethod = ({
     * Start iterating for the max positive number until there are no positive numbers
 
     Second phase:
-    Cut the matrix excluding columns r
+    Copy the matrix from the first phase excluding columns r
     Override the last row with the saved z row
     Solve for a basic solution: x column by x column
     find the row where the column is 1: multiply the last row by those indexes
     if the x columns in the last row are positive stop.
     
+    if minZ
     * Start iterating for the max positive until there are no positive numbers
+    if maxZ
+    * Start iterating for the max positive until there are no negative numbers
+
   */
 
 	/*
     SIMPLEX ALGORITHM
-
-    * Start iterating for the min negative until there are no negative numbers
+    * Start iterating for the max positive until there are no negative numbers
   */
 };
 
